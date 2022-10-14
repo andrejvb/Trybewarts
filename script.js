c32aa414a025bf4dac931ea5238395d8db44bad4
andrejvb <andre.jvb@gmail.com>
2022-10-14 17:50:39 -0300

    requisito 18 completo

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const button = document.getElementById('btn-header');
 const emailHeader = document.getElementById('email-header');
 const passwordHeader = document.getElementById('password-header');
+const buttonSubmit = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
 
 function verificaLogin() {
   if (emailHeader.value === 'tryber@teste.com' && passwordHeader.value === '123456') {
@@ -12,3 +14,8 @@ function verificaLogin() {
 }
 
 button.addEventListener('click', verificaLogin);
+
+checkBox.addEventListener('change', function requiriedCheckBox(){
+  if (checkBox.checked === false) {buttonSubmit.disabled = true  
+  } else {buttonSubmit.disabled = false}
+  })
